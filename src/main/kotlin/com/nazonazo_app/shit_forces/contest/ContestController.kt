42e5cbe174454eb379ef441cd3f542082a0aa64c
ShopOne <oneky8080@gmail.com>
2021-01-28 23:38:51 +0900

    fix time limit close #21

--- src/main/kotlin/com/nazonazo_app/shit_forces/contest/ContestController.kt
+++ src/main/kotlin/com/nazonazo_app/shit_forces/contest/ContestController.kt
@@ -10,7 +10,7 @@ import org.springframework.web.server.ResponseStatusException
 import javax.servlet.http.HttpServletRequest
 
 const val ONE_PAGE_SIZE = 20
-const val SUBMIT_INTERVAL_TIME = 30 * 1000
+const val SUBMIT_INTERVAL_TIME = 10 * 1000
 @RestController
 class ContestController(val contestService: ContestService,
                         val sharedContestService: SharedContestService,
