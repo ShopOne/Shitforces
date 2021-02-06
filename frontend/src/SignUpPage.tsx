27652fbbdbfa06cda5260a2eb13d55f393dd4184
remin <rdrgn@users.noreply.github.com>
2021-02-06 11:50:33 +0900

    refactor(frontend): SignUpPage as a function component

--- frontend/src/SignUpPage.tsx
+++ frontend/src/SignUpPage.tsx
@@ -2,16 +2,14 @@ import React from 'react';
 import { SubmitAccountInfo } from './share-element/SubmitAccountInfo';
 
 // URL: /sighup
-export class SignUpPage extends React.Component {
-  render() {
-    return (
-      <div>
-        <SubmitAccountInfo
-          fetchTo={'/api/signup'}
-          successText={'アカウントの作成に成功しました'}
-          failedText={'アカウントの作成に失敗しました'}
-        />
-      </div>
-    );
-  }
-}
+export const SignUpPage: React.FC = () => {
+  return (
+    <div>
+      <SubmitAccountInfo
+        fetchTo="/api/signup"
+        successText="アカウントの作成に成功しました"
+        failedText="アカウントの作成に失敗しました"
+      />
+    </div>
+  );
+};
