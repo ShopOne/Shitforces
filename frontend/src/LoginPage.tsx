5e230a6a09757025619158ab5f240ba971e50c67
remin <rdrgn@users.noreply.github.com>
2021-02-06 11:51:39 +0900

    refactor(frontend): LoginPage as a function component

--- frontend/src/LoginPage.tsx
+++ frontend/src/LoginPage.tsx
@@ -2,18 +2,15 @@ import React from 'react';
 import { SubmitAccountInfo } from './share-element/SubmitAccountInfo';
 
 // URL: /login
-export class LoginPage extends React.Component {
-  render() {
-    return (
-      <div>
-        <SubmitAccountInfo
-          fetchTo={'/api/login'}
-          successText={'ログインに成功しました'}
-          failedText={
-            'ログインに失敗しました。メールアドレスかパスワードが間違っています'
-          }
-        />
-      </div>
-    );
-  }
-}
+
+export const LoginPage: React.FC = () => {
+  return (
+    <div>
+      <SubmitAccountInfo
+        fetchTo="/api/login"
+        successText="ログインに成功しました"
+        failedText="ログインに失敗しました。メールアドレスかパスワードが間違っています"
+      />
+    </div>
+  );
+};
