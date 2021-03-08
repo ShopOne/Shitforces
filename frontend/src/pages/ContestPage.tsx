b7bbf1f4fb71f8a3130d7c4f7f1ee52da581da4d
ShopOne <oneky8080@gmail.com>
2021-03-08 21:57:27 +0900

    close #106

--- frontend/src/pages/ContestPage.tsx
+++ frontend/src/pages/ContestPage.tsx
@@ -33,8 +33,8 @@ function getContestId() {
 }
 
 function formatSecondToMMSS(ms: number): string {
-  const mm = Math.round(ms / 60);
-  const ss = ('00' + Math.round(ms % 60)).slice(-2);
+  const mm = Math.floor(ms / 60);
+  const ss = ('00' + Math.floor(ms % 60)).slice(-2);
   return `${mm}:${ss}`;
 }
 
