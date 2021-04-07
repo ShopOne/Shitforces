f55c945609be96fd29dd53ee12079146190b44c4
remin <rdrgn@users.noreply.github.com>
2021-04-07 17:16:35 +0900

    feat(frontend): search on standings

--- frontend/src/components/RankingTable.tsx
+++ frontend/src/components/RankingTable.tsx
@@ -1,7 +1,9 @@
-import React, { useMemo } from 'react';
-import { Alert, Table } from 'react-bootstrap';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import { Alert, Button, Form, Pagination, Table } from 'react-bootstrap';
 import { toProblemLabel } from '../functions/toProblemLabel';
 import { ProblemInfo, RankingInfo, RankingInfoAccount } from '../types';
+
+const ACCOUNTS_PER_PAGE = 20;
 
 function formatSecondToMMSS(ms: number): string {
   const mm = Math.floor(ms / 60);
@@ -83,14 +85,83 @@ export const RankingTable: React.FC<Props> = ({
   problems,
   ranking,
 }) => {
-  if (problems.length !== ranking.acPerSubmit.length) {
-    return <Alert variant="danger">Error</Alert>;
-  }
+  const [accountNameToSearch, setAccountNameToSearch] = useState('');
+
+  const onChangeAccountName = useCallback<
+    React.ChangeEventHandler<HTMLInputElement>
+  >((event) => {
+    setAccountNameToSearch(event.target.value);
+  }, []);
+
+  const onClickReset = useCallback(() => {
+    setAccountNameToSearch('');
+  }, []);
 
   const sortedProblems = useMemo(
     () => problems.sort((a, b) => a.indexOfContest - b.indexOfContest),
     [problems]
   );
+
+  const filteredAccounts = useMemo(() => {
+    const uniqueAccounts = new Map<string, RankingInfoAccount>();
+    for (const account of ranking.rankingList) {
+      uniqueAccounts.set(account.accountName, account);
+    }
+
+    let accounts = Array.from(uniqueAccounts.values());
+
+    accounts = accounts.filter((v) =>
+      v.accountName.startsWith(accountNameToSearch)
+    );
+
+    return accounts;
+  }, [ranking.rankingList, accountNameToSearch]);
+
+  const paginationLength = useMemo(
+    () => Math.ceil(filteredAccounts.length / ACCOUNTS_PER_PAGE),
+    [filteredAccounts.length]
+  );
+
+  const [paginationIndex, setPaginationIndex] = useState(0);
+
+  useEffect(() => {
+    if (paginationLength === 0) {
+      if (paginationIndex !== 0) setPaginationIndex(0);
+      return;
+    }
+
+    if (paginationLength <= paginationIndex) {
+      setPaginationIndex(paginationLength - 1);
+    }
+  }, [paginationIndex, paginationLength]);
+
+  const pagenatedAccounts = useMemo(
+    () =>
+      filteredAccounts.filter(
+        (_, i) =>
+          paginationIndex * ACCOUNTS_PER_PAGE <= i &&
+          i < (paginationIndex + 1) * ACCOUNTS_PER_PAGE
+      ),
+    [filteredAccounts, paginationIndex]
+  );
+
+  const paginationItems = useMemo(() => {
+    const items = [];
+    for (let i = 0; i < Math.max(1, paginationLength); i++) {
+      items.push(
+        <Pagination.Item
+          key={i}
+          active={i === paginationIndex}
+          onClick={() => {
+            setPaginationIndex(i);
+          }}
+        >
+          {i + 1}
+        </Pagination.Item>
+      );
+    }
+    return items;
+  }, [paginationIndex, paginationLength]);
 
   const acPerSubmitRow = useMemo(
     () => (
@@ -110,32 +181,61 @@ export const RankingTable: React.FC<Props> = ({
     [ranking.acPerSubmit]
   );
 
+  if (problems.length !== ranking.acPerSubmit.length) {
+    return <Alert variant="danger">Error</Alert>;
+  }
+
   return (
-    <Table size="sm" striped bordered hover responsive>
-      <thead>
-        <tr className="text-center text-nowrap">
-          <th style={{ minWidth: '3em' }}>順位</th>
-          <th style={{ minWidth: '10em' }}>ユーザ</th>
-          <th style={{ minWidth: '4em' }}>得点</th>
-          {sortedProblems.map((problem) => (
-            <th key={problem.id} style={{ minWidth: '4em' }}>
-              {toProblemLabel(problem.indexOfContest)}
-            </th>
-          ))}
-        </tr>
-        {acPerSubmitRow}
-      </thead>
-      <tbody>
-        {ranking.rankingList.map((account, i) => (
-          <RankingTableRow
-            key={i}
-            account={account}
-            problems={sortedProblems}
-            isMe={account.accountName === myAccountName}
+    <>
+      <div className="mb-4">
+        <Form inline>
+          <Form.Label className="mr-2" htmlFor="ranking-table-form-username">
+            ユーザ名
+          </Form.Label>
+          <Form.Control
+            className="mr-2"
+            id="ranking-table-form-username"
+            onChange={onChangeAccountName}
+            value={accountNameToSearch}
           />
-        ))}
-      </tbody>
-      <tfoot>{acPerSubmitRow}</tfoot>
-    </Table>
+          <Button onClick={onClickReset} variant="secondary">
+            リセット
+          </Button>
+        </Form>
+      </div>
+
+      <Table size="sm" striped bordered hover responsive>
+        <thead>
+          <tr className="text-center text-nowrap">
+            <th style={{ minWidth: '3em' }}>順位</th>
+            <th style={{ minWidth: '10em' }}>ユーザ</th>
+            <th style={{ minWidth: '4em' }}>得点</th>
+            {sortedProblems.map((problem) => (
+              <th key={problem.id} style={{ minWidth: '4em' }}>
+                {toProblemLabel(problem.indexOfContest)}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {acPerSubmitRow}
+          {pagenatedAccounts.map((account, i) => (
+            <RankingTableRow
+              key={i}
+              account={account}
+              problems={sortedProblems}
+              isMe={account.accountName === myAccountName}
+            />
+          ))}
+        </tbody>
+        <tfoot>{acPerSubmitRow}</tfoot>
+      </Table>
+
+      <div className="mb-4">
+        <Pagination className="justify-content-center">
+          {paginationItems}
+        </Pagination>
+      </div>
+    </>
   );
 };
