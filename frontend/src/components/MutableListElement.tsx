fb17407d07a8eae15247913c00b207dad96eb114
shop_one <47815703+ShopOne@users.noreply.github.com>
2021-04-09 00:06:43 +0900

    fix problem edit close #162 (#166)

--- frontend/src/components/MutableListElement.tsx
+++ frontend/src/components/MutableListElement.tsx
@@ -1,8 +1,8 @@
-import PropTypes from "prop-types";
-import React, {useEffect, useState} from "react";
-import Col from "react-bootstrap/Col";
-import Form from "react-bootstrap/Form";
-import InputGroup from "react-bootstrap/InputGroup";
+import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 interface MutableListElementProps {
   items: string[];
@@ -12,19 +12,22 @@ export const MutableListElement: React.FC<MutableListElementProps> = ({
   items,
   setItems,
 }) => {
-  const [itemState, setItemState] = useState<{statement: string, id: number}[]>([]);
+  const [itemState, setItemState] = useState<
+    { statement: string; id: number }[]
+  >([]);
   useEffect(() => {
     const itemState = items.map((item, idx) => {
-        return {
-          statement: item,
-          id: idx
-        }});
+      return {
+        statement: item,
+        id: idx,
+      };
+    });
     setItemState(itemState);
   }, []);
   const addItem = () => {
     const newItemState = [...itemState];
     const newItems = [...items];
-    newItemState.push({statement: '', id: newItemState.slice(-1)[0].id});
+    newItemState.push({ statement: '', id: newItemState.slice(-1)[0].id });
     newItems.push('');
     setItemState(newItemState);
     setItems(newItems);
@@ -32,6 +35,9 @@ export const MutableListElement: React.FC<MutableListElementProps> = ({
   const eraseItems = (idx: number) => {
     const newItemState = itemState.filter((_, itemIdx) => idx !== itemIdx);
     const newItems = items.filter((_, itemIdx) => idx !== itemIdx);
+    if (newItems.length === 0) {
+      return;
+    }
     setItemState(newItemState);
     setItems(newItems);
   };
@@ -42,35 +48,35 @@ export const MutableListElement: React.FC<MutableListElementProps> = ({
     newItems[idx] = statement;
     setItemState(newItemState);
     setItems(newItems);
-  }
+  };
   const listElement = () => {
     return itemState.map((item, idx) => {
-      return(
+      return (
         <Form.Row key={item.id}>
           <Col>
             <InputGroup className={'mb-3'}>
               <Form.Control
                 value={item.statement}
-                onChange={(e) => updateItems(idx, e.target.value)}/>
+                onChange={(e) => updateItems(idx, e.target.value)}
+              />
             </InputGroup>
             <Col>
-              <button type={'button'} onClick={addItem}>+</button>
-              <button type={'button'} onClick={() => eraseItems(idx)}>-</button>
+              <button type={'button'} onClick={addItem}>
+                +
+              </button>
+              <button type={'button'} onClick={() => eraseItems(idx)}>
+                -
+              </button>
             </Col>
           </Col>
         </Form.Row>
       );
     });
   };
-  return (
-    <div>
-      {listElement()}
-    </div>
-  );
-
-}
+  return <div>{listElement()}</div>;
+};
 
 MutableListElement.propTypes = {
   items: PropTypes.array.isRequired,
   setItems: PropTypes.func.isRequired,
-}
+};
