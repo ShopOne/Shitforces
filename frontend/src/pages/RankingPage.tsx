e6a07285ae2fa69524b05a4000b43b8945fc3196
shop_one <47815703+ShopOne@users.noreply.github.com>
2021-04-09 22:14:48 +0900

    [WIP] add account page link in RankingPage close #168 (#169)
    * add account page link in RankingPage close #168
    
    * fix link style
    
    * apply Prettier

--- frontend/src/pages/RankingPage.tsx
+++ frontend/src/pages/RankingPage.tsx
@@ -1,23 +1,24 @@
-import React, {useEffect, useState} from "react";
-import Table from "react-bootstrap/Table";
-import { PagingElement } from "../components/PagingElement";
-import { getAccountRankingInfo } from "../functions/HttpRequest";
-import { AccountInfo } from "../types";
+import React, { useEffect, useState } from 'react';
+import Table from 'react-bootstrap/Table';
+import { Link } from 'react-router-dom';
+import { PagingElement } from '../components/PagingElement';
+import { getAccountRankingInfo } from '../functions/HttpRequest';
+import { AccountInfo } from '../types';
 
-const ACCOUNT_RANKING_ONE_PAGE = 20
+const ACCOUNT_RANKING_ONE_PAGE = 20;
 interface AccountRankingTableProps {
   accounts: AccountInfo[];
-  rankStart: number
+  rankStart: number;
 }
 export const AccountRankingTable: React.FC<AccountRankingTableProps> = ({
   accounts,
-  rankStart
+  rankStart,
 }) => {
   const tableBody = (() => {
     const cols = accounts.map((account, idx) => {
       let ratingColor;
       if (account.rating === 0) {
-        ratingColor = 'black'
+        ratingColor = 'black';
       } else if (account.rating < 400) {
         ratingColor = 'gray';
       } else if (account.rating < 800) {
@@ -40,17 +41,17 @@ export const AccountRankingTable: React.FC<AccountRankingTableProps> = ({
       return (
         <tr key={account.name}>
           <td>{idx + rankStart}</td>
-          <td style={{color: ratingColor}}>{account.name}</td>
+          <td>
+            <Link to={`account/${account.name}`} style={{ color: ratingColor }}>
+              {account.name}
+            </Link>
+          </td>
           <td>{account.partNum}</td>
           <td>{account.rating}</td>
         </tr>
       );
-    })
-    return(
-      <tbody>
-      {cols}
-      </tbody>
-    );
+    });
+    return <tbody>{cols}</tbody>;
   })();
   return (
     <Table striped bordered hover>
@@ -64,7 +65,7 @@ export const AccountRankingTable: React.FC<AccountRankingTableProps> = ({
       </thead>
       {tableBody}
     </Table>
-  )
+  );
 };
 export const RankingPage: React.FC = () => {
   const [accounts, setAccounts] = useState<AccountInfo[]>([]);
@@ -72,23 +73,25 @@ export const RankingPage: React.FC = () => {
   const [pageNum, setPageNum] = useState(0);
   const getRanking = (page: number) => {
     getAccountRankingInfo(page)
-      .then(res => {
-        setPageNum(Math.ceil(res.validAccountNum / ACCOUNT_RANKING_ONE_PAGE))
+      .then((res) => {
+        setPageNum(Math.ceil(res.validAccountNum / ACCOUNT_RANKING_ONE_PAGE));
         setNowPage(page);
         setAccounts(res.accounts);
       })
-      .catch(e => {
+      .catch((e) => {
         console.error(e);
       });
   };
-  useEffect( () => {
+  useEffect(() => {
     getRanking(0);
-  } , []);
+  }, []);
   return (
     <>
-      <AccountRankingTable accounts={accounts} rankStart={nowPage * ACCOUNT_RANKING_ONE_PAGE + 1}/>
-      <PagingElement pageChanged={getRanking} pageNum={pageNum}/>
+      <AccountRankingTable
+        accounts={accounts}
+        rankStart={nowPage * ACCOUNT_RANKING_ONE_PAGE + 1}
+      />
+      <PagingElement pageChanged={getRanking} pageNum={pageNum} />
     </>
   );
 };
-
