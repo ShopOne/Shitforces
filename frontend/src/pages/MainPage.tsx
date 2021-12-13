0f26953234d69fcd893654b9080b591c9b640d47
noyan <1.2.3.up.top.charm00ok001996991@gmail.com>
2021-12-13 23:34:03 +0900

    remove layout change

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,13 +1,10 @@
-/* eslint-disable max-lines-per-function */
-import { Box, Flex, Heading, useColorMode, VStack } from '@chakra-ui/react';
+import { useColorMode } from '@chakra-ui/react';
 import { VFC, useEffect, useState, useCallback } from 'react';
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
-import { ContestChakraTable } from '../components/ContestChakraTable';
+import { theme } from '../chakra/theme';
 import { ContestTable } from '../components/ContestTable';
-import { FutureContestChakraTable } from '../components/FutureContextTable';
 import { PagingElement } from '../components/PagingElement';
-import { PastContestChakraTable } from '../components/PastContestChakraTable';
 import {
   getUpcomingContests,
   getActiveContests,
@@ -67,78 +64,39 @@ const ContestList = () => {
 
   return (
     <>
-      <Box my="4" mx="2">
-        <Heading
-          as="h2"
-          fontWeight="thin"
-          size="lg"
-          pl="6"
-          color="orange.500"
-          width="130px"
-          border="4px"
-          borderRadius={'2xl'}
-        >
-          開催中
-        </Heading>
-        {activeContests && activeContests.length > 0 ? (
-          <ContestChakraTable contests={activeContests} />
-        ) : (
-          <div>現在開催中のコンテストはありません</div>
-        )}
-
-        <Heading
-          as="h2"
-          fontWeight="thin"
-          size="lg"
-          pl="6"
-          color="green.500"
-          width="100px"
-          border="4px"
-          borderRadius={'2xl'}
-          mt="12"
-        >
-          予定
-        </Heading>
-        <FutureContestChakraTable contests={upcomingContests} />
-
-        <Heading
-          as="h2"
-          fontWeight="thin"
-          size="lg"
-          pl="6"
-          color="gray.500"
-          width="100px"
-          border="4px"
-          borderRadius={'2xl'}
-          mt="12"
-        >
-          終了
-        </Heading>
-        <PastContestChakraTable contests={pastContests} />
-        <Box display="flex" justifyContent="center" alignItems="center" mt="4">
-          <PagingElement
-            currentPage={contestCurrentPage}
-            onChange={updateContestPage}
-            savePaging={true}
-            totalPages={contestPageNum}
-          />
-        </Box>
-        {/*
-        pageのクエリパラメータを全てで使っているので、同じページに二つPagingElementがあるとページが同期しちゃう
-        というか上位10~20人だけでもいいかも
-        <h2>ランキング</h2>
-        <Ranking />
-        */}
-      </Box>
-      <Box mt="12">
-        <Link to={'/ranking'}>
-          <Button variant={'primary'}>順位表へ</Button>
-        </Link>
-      </Box>
+      <h2>開催中のコンテスト</h2>
+      {activeContests && activeContests.length > 0 ? (
+        <ContestTable contests={activeContests} />
+      ) : (
+        <div>現在開催中のコンテストはありません</div>
+      )}
+      <h2>予定されたコンテスト</h2>
+      <ContestTable contests={upcomingContests} />
+      <h2>終了したコンテスト</h2>
+      <ContestTable contests={pastContests} />
+      <PagingElement
+        currentPage={contestCurrentPage}
+        onChange={updateContestPage}
+        savePaging={true}
+        totalPages={contestPageNum}
+      />
+      {/*
+      pageのクエリパラメータを全てで使っているので、同じページに二つPagingElementがあるとページが同期しちゃう
+      というか上位10~20人だけでもいいかも
+      <h2>ランキング</h2>
+      <Ranking />
+      */}
+      <Link to={'/ranking'}>
+        <Button variant={'primary'}>順位表へ</Button>
+      </Link>
     </>
   );
 };
 
 export const MainPage: VFC = () => {
-  return <ContestList />;
+  return (
+    // <ChakraProvider theme={theme}>
+    <ContestList />
+    // </ChakraProvider>
+  );
 };
